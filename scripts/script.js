617bc3fd88faeb605de3ebcb4287f416291b27a5
Marco Kuan <mkuan@ucsd.edu>
2021-02-11 04:18:14 -0800

    make fetch await before creation

--- scripts/script.js
+++ scripts/script.js
@@ -2,50 +2,71 @@
 var localStorage = window.localStorage;
 var addedList = [];
 
-window.addEventListener('DOMContentLoaded', () => {
-  if (localStorage.getItem('items') == null) {
-    // Fetch data (json) from website
-    fetch('https://fakestoreapi.com/products')
-      .then(response => response.json())
-      .then(data => {
-        // Store the data
-        localStorage.setItem('items', JSON.stringify(data));
-      });
-
-    console.log('Items stored');
+window.addEventListener("DOMContentLoaded", () => {
+  if (localStorage.getItem("items") == null) {
+    // Fetch data (json) from website and create the items
+    fetchAndCreate();
   } else {
-    console.log('Items already exist');
+    // List the items from the existing localStorage
+    createItems();
   }
+});
 
+/**
+ * An asynchronous function that grabs data from the url link
+ */
+async function fetchAndCreate() {
+  const response = await fetch("https://fakestoreapi.com/products");
+  const json = await response.json();
+
+  localStorage.setItem("items", JSON.stringify(json));
+
+  await createItems();
+}
+
+/**
+ * Helper asynchronous function that creates the items and puts them into the
+ * shop
+ */
+async function createItems() {
   // Create all the items in the shop
-  let items = JSON.parse(localStorage.getItem('items'));
-  let productList = document.getElementById('product-list');
+  let items = JSON.parse(localStorage.getItem("items"));
+  let productList = document.getElementById("product-list");
 
-  // Load the item 
-  if (localStorage.getItem('itemCnt')) {
+  // Load the item
+  if (localStorage.getItem("itemCnt")) {
     // Load saved cart data
-    let count = document.getElementById('cart-count');
+    let count = document.getElementById("cart-count");
 
-    addedList = JSON.parse(localStorage.getItem('itemCnt'));
+    addedList = JSON.parse(localStorage.getItem("itemCnt"));
     count.innerHTML = addedList.length;
   } else {
     // Use an empty cart
-    localStorage.setItem('itemCnt', addedList);
+    localStorage.setItem("itemCnt", addedList);
   }
 
-
+  // Iterate through all the items (create the cards) and put them into the shop
   for (i in items) {
     let prodItem;
 
     if (addedList.includes(items[i].id)) {
-      prodItem = new ProductItem(items[i].title, items[i].price,
-        items[i].image, items[i].id, 'Remove from Cart');
+      prodItem = new ProductItem(
+        items[i].title,
+        items[i].price,
+        items[i].image,
+        items[i].id,
+        "Remove from Cart"
+      );
     } else {
-      prodItem = new ProductItem(items[i].title, items[i].price,
-        items[i].image, items[i].id, 'Add to Cart');
+      prodItem = new ProductItem(
+        items[i].title,
+        items[i].price,
+        items[i].image,
+        items[i].id,
+        "Add to Cart"
+      );
     }
 
     productList.appendChild(prodItem);
   }
-})
-
+}
